9028f31d5a7509bc29db1d83f8282d4f38ca7f28
Myesha <myeshaazkaa@gmail.com>
2026-09-05 13:29:56 +0700

    ubah index.js

--- src/index.js
+++ src/index.js
@@ -14,4 +14,12 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/customers", customerRoutes);
 
+const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
 export default app;
